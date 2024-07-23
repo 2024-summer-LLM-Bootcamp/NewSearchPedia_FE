78622bdf35f6684e77697d0bc953cc93c9123127
daesung <dasung56@gmail.com>
2024-07-23 10:48:33 +0900

    아티클 목록 사이드바 추가

--- src/pages/article/PrimarySearchAppBar.jsx
+++ src/pages/article/PrimarySearchAppBar.jsx
@@ -9,12 +9,13 @@ import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import TemporaryDrawer from './drawer';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -149,7 +150,8 @@ export default function PrimarySearchAppBar() {
       <AppBar position="static">
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }}>
-            <MenuIcon />
+            {/* <MenuIcon /> */}
+            <TemporaryDrawer/>
           </IconButton>
           <Typography variant="h6" noWrap component="div" sx={{ display: { xs: 'none', sm: 'block' } }}>
             NewSearchManager
