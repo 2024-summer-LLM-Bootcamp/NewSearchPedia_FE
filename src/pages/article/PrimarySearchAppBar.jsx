e5dbd93495064a5e706803545b49f12f72274ebe
daesung <dasung56@gmail.com>
2024-07-23 15:03:18 +0900

    AppBar에서 불필요한 아이콘 삭제

--- src/pages/article/PrimarySearchAppBar.jsx
+++ src/pages/article/PrimarySearchAppBar.jsx
@@ -107,19 +107,9 @@ export default function PrimarySearchAppBar() {
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
         <p>Messages</p>
       </MenuItem>
       <MenuItem>
-        <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
         <p>Notifications</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
@@ -150,16 +140,6 @@ export default function PrimarySearchAppBar() {
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
             <IconButton size="large" edge="end" aria-label="account of current user" aria-controls={menuId} aria-haspopup="true" onClick={handleProfileMenuOpen} color="inherit">
               <AccountCircle />
             </IconButton>
--- src/pages/article/drawer.jsx
+++ src/pages/article/drawer.jsx
@@ -5,68 +5,63 @@ import Pagination from '@mui/material/Pagination';
 import ArticleSearch from './articleSearch';
 
 export default function TemporaryDrawer() {
-    const [open, setOpen] = React.useState(false);
-    const [currentPage, setCurrentPage] = React.useState(1);
-  
-    const itemsPerPage = 4; // 페이지당 항목 수
+  const [open, setOpen] = React.useState(false);
+  const [currentPage, setCurrentPage] = React.useState(1);
 
-    //서버에서 받아올? 사용자 입력값 (예시)
-    const items = ['Inbox', 'Starred', 'Send email', 'Drafts', 'All mail', 'Trash', 'Spam'];
-    
-    const handleChangePage = (event, newPage) => {
-      setCurrentPage(newPage);
-    };
-  
-    const toggleDrawer = (newOpen) => () => {
-      setOpen(newOpen);
-    };
-  
-    const preventClickPropagation = (event) => {
-      event.stopPropagation();
-    };
-  
-    const paginatedItems = items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-  
-    const DrawerList = (
-      <Box sx={{ width: 250 }} role="presentation" onClick={preventClickPropagation}>
-        <ArticleSearch />
-        <List>
-          {paginatedItems.map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <Pagination
-          count={Math.ceil(items.length / itemsPerPage)}
-          page={currentPage}
-          onChange={handleChangePage}
-          sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}
-        />
-      </Box>
-    );
-  
-    return (
-      <div>
-        <Button
-          onClick={toggleDrawer(true)}
-          sx={{
-            color: '#ffffff',
-            backgroundColor: '#3f51b5',
-            '&:hover': {
-              backgroundColor: '#303f9f',
-            },
-          }}
-        >
-          <MenuIcon />
-        </Button>
-        <Drawer open={open} onClose={toggleDrawer(false)}>
-          {DrawerList}
-        </Drawer>
-      </div>
-    );
-  }
+  const itemsPerPage = 4; // 페이지당 항목 수
+
+//   서버에서 받아올? 사용자 입력값 (예시)
+  const items = ['Inbox', 'Starred', 'Send email', 'Drafts', 'All mail', 'Trash', 'Spam'];
+
+  const handleChangePage = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const preventClickPropagation = (event) => {
+    event.stopPropagation();
+  };
+
+  const paginatedItems = items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={preventClickPropagation}>
+      <ArticleSearch />
+      <List>
+        {paginatedItems.map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <Pagination count={Math.ceil(items.length / itemsPerPage)} page={currentPage} onChange={handleChangePage} sx={{ display: 'flex', justifyContent: 'center', mt: 2 }} />
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button
+        onClick={toggleDrawer(true)}
+        sx={{
+          color: '#ffffff',
+          backgroundColor: '#3f51b5',
+          '&:hover': {
+            backgroundColor: '#303f9f',
+          },
+        }}
+      >
+        <MenuIcon />
+      </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
